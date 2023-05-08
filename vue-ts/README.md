37eb652302abfdb039c6aeb85b428001e20cd8cb
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2023-05-08 21:05:21 +0800

    vue-ts

---
+++ vue-ts/README.md
@@ -0,0 +1,11 @@
+# vue-ts template
+
+- vue-router
+- pinia
+- @vueuse/core
+- axios
+- eslint
+- prettier
+- unocss
+- sass
+- vitest
---
+++ vue-ts/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Suspense>
+    <RouterView></RouterView>
+  </Suspense>
+</template>
